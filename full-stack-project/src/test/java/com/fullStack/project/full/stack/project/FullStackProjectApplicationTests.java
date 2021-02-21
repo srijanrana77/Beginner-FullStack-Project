20ecc70095a0141e7ca4f66a106ecbafcda8dd83
srijanrana77 <70372578+srijanrana77@users.noreply.github.com>
2021-02-21 19:44:08 +0530

    initial

---
+++ full-stack-project/src/test/java/com/fullStack/project/full/stack/project/FullStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStack.project.full.stack.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
